fcbd9d81941f5751907e8d36e48bccdaeeac5396
cryptofish <yocryptofish7@gmail.com>
2021-10-16 10:30:21 -0700

    Revamp Maker subgraph

---
+++ subgraphs/maker/src/entities/server.ts
@@ -0,0 +1,24 @@
+import { Server } from '../../generated/schema'
+import { Address, ethereum } from '@graphprotocol/graph-ts'
+import { BIG_DECIMAL_ZERO, BIG_INT_ZERO } from 'const'
+import { getMaker } from './maker'
+
+export function getServer(address: Address, block: ethereum.Block): Server {
+  const maker = getMaker(block)
+
+  let server = Server.load(address.toHex())
+
+  if (server === null) {
+    server = new Server(address.toHex())
+    server.maker = maker.id
+    server.joeServed = BIG_DECIMAL_ZERO
+    server.joeServedUSD = BIG_DECIMAL_ZERO
+    server.totalServings = BIG_INT_ZERO
+  }
+
+  server.timestamp = block.timestamp
+  server.block = block.number
+  server.save()
+
+  return server as Server
+}
